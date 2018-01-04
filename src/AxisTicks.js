fff98e97eec988ec0b2c5981693d7388d03450e8
Abhishek Pratapa <npratapa@apple.com>
2018-01-05 04:20:27 +0530

    updated axis code to have an integer flag for only displaying integers on an axis

--- src/AxisTicks.js
+++ src/AxisTicks.js
@@ -32,7 +32,8 @@ prototype.transform = function(_, pulse) {
   var out = pulse.fork(pulse.NO_SOURCE | pulse.NO_FIELDS),
       ticks = this.value,
       scale = _.scale,
-      count = _.count == null ? (_.values ? _.values.length : 10) : tickCount(scale, _.count),
+      // TODO: Change the the true to a value that's a flag in the axis section of the "vega spec"
+      count = _.count == null ? (_.values ? _.values.length : (_.integer == null || !_.integer)?10:((scale.domain()[1] < 10)?scale.domain()[1]:10)) : tickCount(scale, _.count),
       format = _.format || tickFormat(scale, count, _.formatSpecifier),
       values = _.values ? validTicks(scale, _.values, count) : tickValues(scale, count);
 
